0325d71910da5407224d8c0b71aeb7dff9eebd41
Thomas Remblier <thomas.rembl@gmail.com>
2024-07-22 10:43:37 +0200

    clear

--- next.config.js
+++ next.config.js
@@ -3,13 +3,6 @@ const nextConfig = {
   images: {
     domains: ["utfs.io"],
   },
-  env: {
-    NEXT_PUBLIC_CLERK_FRONTEND_API: process.env.NEXT_PUBLIC_CLERK_FRONTEND_API,
-    CLERK_API_KEY: process.env.CLERK_API_KEY,
-    NEXT_PUBLIC_CLERK_SIGN_IN_URL: 'https://account.liftdigital.fr/sign-in',
-    NEXT_PUBLIC_CLERK_SIGN_UP_URL: 'https://account.liftdigital.fr/sign-up',
-    NEXT_PUBLIC_CLERK_ISSUER: 'https://account.liftdigital.fr',
-  },
 };
 
 module.exports = nextConfig;
